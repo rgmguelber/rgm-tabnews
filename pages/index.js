913e1d8e9833b4609f5c2c04f5a2f116aea3f436
Ramon Guelber Martins <rgmguelber@gmail.com>
2025-08-11 21:36:46 -0300

    Instalação do pacote Next, React e React-dom e configuração do `.gitignore`

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to RGM TabNews</h1>
+      <p>
+        This is a clone of TabNews by Felipe Deschamps, created following the
+        curso.dev
+      </p>
+    </div>
+  );
+}
+
+export default Home;
