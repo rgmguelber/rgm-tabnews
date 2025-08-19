4fcd3ddb3eb8123ce4a71c70e2c8674d5acbdcac
Ramon Guelber Martins <rgmguelber@gmail.com>
2025-08-18 21:29:03 -0300

    Add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,10 @@
-function getStatus(req, res) {
+import database from "../../../../infra/database.js";
+
+async function getStatus(req, res) {
+  const result = await database.query("SELECT 1+1;");
+
+  console.log(result.rows);
+
   res.status(200).json({ status: "OK" });
 }
 
