88fc63a3e2fca45fb459c9718d1f17d49e117896
Ramon Guelber Martins <44545912+rgmguelber@users.noreply.github.com>
2025-09-01 17:59:36 +0000

    test: refact `tests/integrations/api/v1/status`

--- tests/integrations/api/v1/status/get.test.js
+++ tests/integrations/api/v1/status/get.test.js
@@ -4,19 +4,26 @@ beforeAll(async () => {
   await orchestrator.waitForAllServices();
 });
 
-test("GET to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe(200);
+describe("GET to /api/v1/status", () => {
+  describe("Anonymous user", () => {
+    test("Retrive current system status", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+      expect(response.status).toBe(200);
 
-  const responseBody = await response.json();
+      const responseBody = await response.json();
 
-  // Testa se o campo updated_at está presente e é uma data válida
-  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
-  expect(parsedUpdatedAt).toBe(responseBody.updated_at);
+      // Testa se o campo updated_at está presente e é uma data válida
+      const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+      expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 
-  // Testa se o campo database.version retorna a versão correta
-  expect(responseBody.dependencies.database.version).toEqual("16.0");
+      // Testa se o campo database.version retorna a versão correta
+      expect(responseBody.dependencies.database.version).toEqual("16.0");
 
-  // Testa se o campo dependencies.database.max_connections está presente
-  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+      // Testa se o campo dependencies.database.max_connections está presente
+      expect(responseBody.dependencies.database.max_connections).toEqual(100);
+
+      // Verifica a quantidade de conexões ativas
+      expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+    });
+  });
 });
