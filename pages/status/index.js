4c6d79d92c26974b7b5f4c7878b07059024a75df
Ramon Guelber Martins <44545912+rgmguelber@users.noreply.github.com>
2025-09-03 20:18:49 +0000

    feat: add `status` page

--- pages/status/index.js
+++ pages/status/index.js
@@ -1,13 +1,15 @@
 import useSWR from "swr";
 
-async function fetchStatus() {
-  const response = await fetch("/api/v1/status");
+async function fetchAPI(key) {
+  const response = await fetch(key);
   const responseBody = await response.json();
   return responseBody;
 }
 
 export default function StatusPage() {
-  const response = useSWR("status", fetchStatus);
+  const { data, isLoading } = useSWR("/api/v1/status", fetchAPI, {
+    refreshInterval: 5000,
+  });
 
   return (
     <>
@@ -16,28 +18,26 @@ export default function StatusPage() {
       <div>
         <p>
           <strong>Data Atualização: </strong>
-          {response.isLoading ? "Carregando" : response.data.updated_at}
+          {isLoading ? "Carregando..." : data.updated_at}
         </p>
 
         <p>
           <strong>Versão do Postgres: </strong>
-          {response.isLoading
-            ? "Carregando"
-            : response.data.dependencies.database.version}
+          {isLoading ? "Carregando..." : data.dependencies.database.version}
         </p>
 
         <p>
           <strong>Conexões Máximas: </strong>
-          {response.isLoading
-            ? "Carregando"
-            : response.data.dependencies.database.max_connections}
+          {isLoading
+            ? "Carregando..."
+            : data.dependencies.database.max_connections}
         </p>
 
         <p>
           <strong>Conexões Abertas: </strong>
-          {response.isLoading
-            ? "Carregando"
-            : response.data.dependencies.database.opened_connections}
+          {isLoading
+            ? "Carregando..."
+            : data.dependencies.database.opened_connections}
         </p>
       </div>
     </>
